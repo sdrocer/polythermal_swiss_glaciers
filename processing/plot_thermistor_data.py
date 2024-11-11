8244b983c016abb857e26a5a16905a30e909c4ff
sdrocer <janosch_beer@gmx.de>
2024-11-11 17:55:20 +0100

    plot calibration data

--- processing/plot_thermistor_data.py
+++ processing/plot_thermistor_data.py
@@ -2,8 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # import self-written modules
-from process_thermistor_data import ThermistorData
-from process_thermistor_data import ThermistorDataPlotter
+os.chdir('/Users/janoschbeer/Library/Mobile Documents/com~apple~CloudDocs/PhD/projects/asses_swiss_gl_therm_regimes/')
+from processing.process_thermistor_data import ThermistorData
+from processing.process_thermistor_data import ThermistorDataPlotter
+from calibration.thermistor_chains_0deg_references import *
 
 """
     Python script to plot different types of thermistor data.
@@ -57,3 +59,60 @@ hohlaub.plot_multiple_ntc_boreholes(savepath=output_path  , title='Hohlaubgletsc
 chessjen.plot_multiple_ntc_boreholes(savepath=output_path , title='Chessjengletscher 08/2024-09/2024'   , depths=[6.8,11.8,8.5,13.5]    , borehole_labels=['BH7','BH8'], lower_y_limit=-1.5, legend_loc='upper right')
 alphubel.plot_multiple_ntc_boreholes(savepath=output_path , title='Alphubel South 08/2024-10/2024'      , depths=[8.0,13.0,9.0,14.0]    , borehole_labels=['BH9','BH10'], lower_y_limit=-3.5)
 corvatsch.plot_multiple_ntc_boreholes(savepath=output_path, title='Corvatsch 08/2024-10/2024'           , depths=[2.0,7.0,4.3,9.3]      , borehole_labels=['BH11','BH12'], lower_y_limit=-3.5)
+
+## Plot calibration data ##
+# ----------------------- #
+
+# set the paths to the calibration data
+logger_1_dir = cal_path + 'raw_files/#1_ice_bath_0deg_offset_second_trial.csv'
+logger_2_dir = cal_path + 'raw_files/#2_ice_bath_0deg_offset_second_trial.csv'
+logger_3_dir = cal_path + 'raw_files/#3_ice_bath_0deg_offset_second_trial.csv'
+logger_4_dir = cal_path + 'raw_files/#4_ice_bath_0deg_offset_second_trial.csv'
+logger_5_dir = cal_path + 'raw_files/#5_ice_bath_0deg_offset_second_trial.csv'
+logger_6_dir = cal_path + 'raw_files/#6_ice_bath_0deg_offset_second_trial.csv'
+logger_7_dir = cal_path + 'raw_files/#7_ice_bath_0deg_offset_second_trial.csv'
+logger_8_dir = cal_path + 'raw_files/#8_ice_bath_0deg_offset_second_trial.csv'
+logger_9_dir = cal_path + 'raw_files/#9_ice_bath_0deg_offset.csv'
+logger_10_dir = cal_path + 'raw_files/#10_ice_bath_0deg_offset.csv'
+logger_11_dir = cal_path + 'raw_files/#11_ice_bath_0deg_offset.csv'
+logger_12_dir = cal_path + 'raw_files/#12_ice_bath_0deg_offset.csv'
+logger_13_dir = cal_path + 'raw_files/#13_ice_bath_0deg_offset.csv'
+logger_14_dir = cal_path + 'raw_files/#14_ice_bath_0deg_offset.csv'
+logger_15_dir = cal_path + 'raw_files/#15_ice_bath_0deg_offset_black_probe_missing.csv'
+logger_16_dir = cal_path + 'raw_files/#16_ice_bath_0deg_offset.csv'
+
+# create a ThermistorDataPlotter object per logger
+logger_1 = ThermistorDataPlotter(logger_1_dir, delimiter=',')
+logger_2 = ThermistorDataPlotter(logger_2_dir, delimiter=',')
+logger_3 = ThermistorDataPlotter(logger_3_dir, delimiter=',')
+logger_4 = ThermistorDataPlotter(logger_4_dir, delimiter=',')
+logger_5 = ThermistorDataPlotter(logger_5_dir, delimiter=',')
+logger_6 = ThermistorDataPlotter(logger_6_dir, delimiter=',')
+logger_7 = ThermistorDataPlotter(logger_7_dir, delimiter=',')
+logger_8 = ThermistorDataPlotter(logger_8_dir, delimiter=',')
+logger_9 = ThermistorDataPlotter(logger_9_dir, delimiter=',')
+logger_10 = ThermistorDataPlotter(logger_10_dir, delimiter=',')
+logger_11 = ThermistorDataPlotter(logger_11_dir, delimiter=',')
+logger_12 = ThermistorDataPlotter(logger_12_dir, delimiter=',')
+logger_13 = ThermistorDataPlotter(logger_13_dir, delimiter=',')
+logger_14 = ThermistorDataPlotter(logger_14_dir, delimiter=',')
+logger_15 = ThermistorDataPlotter(logger_15_dir, delimiter=',')
+logger_16 = ThermistorDataPlotter(logger_16_dir, delimiter=',')
+
+# plot the data per logger
+logger_1.plot_ntc_icebath_calibration(data_10m_chain_2nd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #1 - 0deg offset in ice bath')
+logger_2.plot_ntc_icebath_calibration(data_10m_chain_2nd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #2 - 0deg offset in ice bath')
+logger_3.plot_ntc_icebath_calibration(data_10m_chain_2nd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #3 - 0deg offset in ice bath')
+logger_4.plot_ntc_icebath_calibration(data_10m_chain_2nd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #4 - 0deg offset in ice bath', y_limits=[-7,7])
+logger_5.plot_ntc_icebath_calibration(data_10m_chain_2nd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #5 - 0deg offset in ice bath')
+logger_6.plot_ntc_icebath_calibration(data_10m_chain_2nd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #6 - 0deg offset in ice bath')
+logger_7.plot_ntc_icebath_calibration(data_10m_chain_2nd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #7 - 0deg offset in ice bath', y_limits=[-7,7])
+logger_8.plot_ntc_icebath_calibration(data_10m_chain_2nd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #8 - 0deg offset in ice bath')
+logger_9.plot_ntc_icebath_calibration(data_10m_chain_3rd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #9 - 0deg offset in ice bath')
+logger_10.plot_ntc_icebath_calibration(data_10m_chain_3rd_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #10 - 0deg offset in ice bath')
+logger_11.plot_ntc_icebath_calibration(data_10m_chain_4th_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #11 - 0deg offset in ice bath')
+logger_12.plot_ntc_icebath_calibration(data_10m_chain_4th_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #12 - 0deg offset in ice bath')
+logger_13.plot_ntc_icebath_calibration(data_10m_chain_4th_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #13 - 0deg offset in ice bath')
+logger_14.plot_ntc_icebath_calibration(data_10m_chain_4th_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #14 - 0deg offset in ice bath')
+logger_15.plot_ntc_icebath_calibration(data_10m_chain_4th_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #15 - 0deg offset in ice bath, no black probe')
+logger_16.plot_ntc_icebath_calibration(data_10m_chain_4th_ice_bath, savepath=output_path + 'thermistor_calibration/', title='Logger #16 - 0deg offset in ice bath')
